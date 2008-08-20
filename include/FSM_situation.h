a4d0ad2e3b4810a3b3fd20ae2e1b07e9ac6ae0b6
Julien Thevenon <julien_thevenon@yahoo.fr>
2008-08-20 11:06:39 +0200

    Update to add method to manage situation validity

--- include/FSM_situation.h
+++ include/FSM_situation.h
@@ -6,22 +6,25 @@
 
 template <class T_CONTEXT> class FSM_situation:public FSM_situation_if
 {
-	public:
-		// Constructors and destructors
-		FSM_situation(void);
-		FSM_situation(const FSM_situation &p_situation);
-		virtual ~FSM_situation(void);
+ public:
+  // Constructors and destructors
+  FSM_situation(void);
+  FSM_situation(const FSM_situation &p_situation);
+  virtual ~FSM_situation(void);
 
-		// Methods inherited from interface
-		FSM_context_if* getCurrentContext(void)const;
-		virtual string toString(void)const=0;
+  // Methods inherited from interface
+  FSM_context_if* getCurrentContext(void)const;
+  virtual string toString(void)const=0;
+  bool isValid(void)const;
+  void setInvalid(void);
 
-		// Specific methods
-		T_CONTEXT* getContext(void)const;
-		void setContext(T_CONTEXT *p_context);
+  // Specific methods
+  T_CONTEXT* getContext(void)const;
+  void setContext(T_CONTEXT *p_context);
 
-	private:
-		T_CONTEXT *m_context;
+ private:
+  T_CONTEXT *m_context;
+  bool m_valid;
 };
 
 #include "FSM_situation.hpp"
--- include/FSM_situation.hpp
+++ include/FSM_situation.hpp
@@ -1,50 +1,69 @@
 //-----------------------------------------------------------------------------
 template <class T_CONTEXT>
 FSM_situation<T_CONTEXT>::FSM_situation(void):
-  m_context(NULL)  
+  m_context(NULL),
+  m_valid(true)
 {
 }
 
 //-----------------------------------------------------------------------------
 template <class T_CONTEXT>
-FSM_situation<T_CONTEXT>::FSM_situation(const FSM_situation &p_situation)
+FSM_situation<T_CONTEXT>::FSM_situation(const FSM_situation &p_situation):
+  m_context(new T_CONTEXT(*(p_situation.getContext()))),
+  m_valid(p_situation.isValid())
 {
-  m_context = new T_CONTEXT(*(p_situation.getContext()));
 }
 
 //-----------------------------------------------------------------------------
 template <class T_CONTEXT>
-	FSM_situation<T_CONTEXT>::~FSM_situation(void)
-	{
-		if(m_context != NULL)
-		{
-			delete m_context ;
-			m_context = NULL;
-		}
-	}
+FSM_situation<T_CONTEXT>::~FSM_situation(void)
+{
+  if(m_context != NULL)
+    {
+      delete m_context ;
+      m_context = NULL;
+    }
+}
 
 //-----------------------------------------------------------------------------
 template <class T_CONTEXT>
-	FSM_context_if* FSM_situation<T_CONTEXT>::getCurrentContext(void)const
-	{
-		return m_context;
-	}
+FSM_context_if* FSM_situation<T_CONTEXT>::getCurrentContext(void)const
+{
+  return m_context;
+}
 
 //-----------------------------------------------------------------------------
 template <class T_CONTEXT>
-	T_CONTEXT* FSM_situation<T_CONTEXT>::getContext(void)const
-	{
-		return m_context;
-	}
+bool FSM_situation<T_CONTEXT>::isValid(void)const
+{
+  return m_valid;
+}
 
 //-----------------------------------------------------------------------------
 template <class T_CONTEXT>
-	void FSM_situation<T_CONTEXT>::setContext
-	(
-		T_CONTEXT *p_context
-	)
-	{
-		assert(p_context != NULL);
-		m_context = p_context;
-	}
+T_CONTEXT* FSM_situation<T_CONTEXT>::getContext(void)const
+{
+  return m_context;
+}
+
+//-----------------------------------------------------------------------------
+template <class T_CONTEXT>
+void FSM_situation<T_CONTEXT>::setContext
+(
+ T_CONTEXT *p_context
+ )
+{
+  assert(p_context != NULL);
+  m_context = p_context;
+}
+
+//-----------------------------------------------------------------------------
+template <class T_CONTEXT>
+void FSM_situation<T_CONTEXT>::setInvalid
+(
+ void
+ )
+{
+  m_valid = false;
+}
 
