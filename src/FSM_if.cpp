23ccab05b7931d81ac298152010f2d67fc2e464b
Julien Thevenon <julien_thevenon@yahoo.fr>
2008-08-27 22:34:03 +0200

    Adding using namespace removed from FSM_interfaces headers

--- src/FSM_if.cpp
+++ src/FSM_if.cpp
@@ -1,5 +1,7 @@
 #include "FSM_if.h"
 
+using namespace std;
+
 //-----------------------------------------------------------------------------
 FSM_if::~FSM_if(void)
 {
@@ -8,10 +10,10 @@ FSM_if::~FSM_if(void)
 //-----------------------------------------------------------------------------
 void registerFsm
 (
-	string p_FSM_type,
-	FSM_creator p_creator,
-	map<string,FSM_creator> *p_factory
-)
+ string p_FSM_type,
+ FSM_creator p_creator,
+ map<string,FSM_creator> *p_factory
+ )
 {
-	p_factory->insert(map<string,FSM_creator>::value_type(p_FSM_type,p_creator));
+  p_factory->insert(map<string,FSM_creator>::value_type(p_FSM_type,p_creator));
 }
